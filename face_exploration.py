f8d1c4ed50ac7c901c4c57764df11ecaa2ead317
Alliyah Steele <alliyah@oligo.space>
2026-07-30 16:53:35 -0700

    FEAT: Rudimentary hole detection pre-curser

--- face_exploration.py
+++ face_exploration.py
@@ -1,61 +1,28 @@
-contacts = DataModel.GetObjectsByType(DataModelObjectCategory.ContactRegion)
-for c in contacts:
-    print(c.Name, "| source:", c.SourceLocation.Name, "| target:", c.TargetLocation.Name)
+for part in Model.Geometry.Children: 
+    for body in part.Children:
+        print(part.Name, body.Name, type(body))
 
+one_body = Model.Geometry.Children[0].Children[0]
+geo_body = one_body.GetGeoBody()
+print(geo_body)
+faces = geo_body.Faces
+print(faces)
+print(len(faces))
 
-"""
-Model
-  Geometry Imports
-    Geometry Import
-  Geometry
-    Solid
-      Solid
-    Solid
-      Solid
-    Solid
-      Solid
-    Solid
-      Solid
-    Solid
-      Solid
-    Solid
-      Solid
-  Materials
-    Structural Steel
-  Coordinate Systems
-    Global Coordinate System
-  Remote Points
-  Connections
-    Contacts
-      Contact Region
-      Contact Region 2
-      Contact Region 3
-      Contact Region 4
-      Contact Region 5
-      Contact Region 6
-      Contact Region 7
-      Contact Region 8
-      Contact Region 9
-  Mesh
-  Named Selections
-    block3_block2_cont
-    block3_block2_targ
-    shank_block3_targ
-    shank_block3_cont
-    block1_washer_cont
-    block1_washer_targ
-    washer_bolt_cont
-    washer_bolt_targ
-    shank_bolt_targ
-    shank_bolt_cont
-    block2_block1_cont
-    block2_block1_targ
-    all_bodies
-    bodies_5
-    shank
-    shank_face
-    shank_face2
-    bottom_surface
-    block2_surface
-    shank_surface
-"""
+sample_face = faces[0]
+print(type(sample_face))
+clr_type = sample_face.GetType()
+for member in clr_type.GetProperties():
+    print(member.Name)
+
+for f in faces:
+    print(f.Id, f.SurfaceType)
+
+contact = DataModel.GetObjectsByType(DataModelObjectCategory.ContactRegion)[0]
+source_ids = list(contact.SourceLocation.Ids)
+print(source_ids)
+
+matching_faces = [f for f in faces if f.Id in source_ids]
+print(len(matching_faces))
+
+#NOTE: Looking for: Ansys.ACT.Interfaces.Geometry.GeoSurfaceTypeEnum.GeoSurfaceCylinder
